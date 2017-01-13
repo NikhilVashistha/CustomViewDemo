48d68f9170d27a8fc09f8e9aad6fff828e609d3f
Nikhil Vashistha <nikhil@atlogys.com>
2017-01-13 19:16:57 +0530

    showing random no.

--- app/src/main/java/com/ndroidpro/customviewdemo/MainActivity.java
+++ app/src/main/java/com/ndroidpro/customviewdemo/MainActivity.java
@@ -1,37 +1,75 @@
 package com.ndroidpro.customviewdemo;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity implements RecordTocoEcgView.notifyScrolledListener {
+
+    private Handler handler;
+    RecordTocoEcgView monitorTocoEcgView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Listener.TimeData timeDataLinkedList[]= new Listener.TimeData[10];
-        Listener.TimeData timeData = new Listener.TimeData(100,120);
-        Listener.TimeData timeData1 = new Listener.TimeData(110,130);
-        Listener.TimeData timeData12 = new Listener.TimeData(120,140);
-        Listener.TimeData timeData13 = new Listener.TimeData(130,150);
-        Listener.TimeData timeData14 = new Listener.TimeData(140,160);
-        Listener.TimeData timeData15 = new Listener.TimeData(150,170);
-        Listener.TimeData timeData16 = new Listener.TimeData(160,180);
-        Listener.TimeData timeData17 = new Listener.TimeData(170,190);
-        Listener.TimeData timeData18 = new Listener.TimeData(180,200);
-        Listener.TimeData timeData19 = new Listener.TimeData(190,210);
 
-        timeDataLinkedList[0] = timeData;
-        timeDataLinkedList[1] = timeData1;
-        timeDataLinkedList[2] = timeData12;
-        timeDataLinkedList[3] = timeData13;
-        timeDataLinkedList[4] = timeData14;
-        timeDataLinkedList[5] = timeData15;
-        timeDataLinkedList[6] = timeData16;
-        timeDataLinkedList[7] = timeData17;
-        timeDataLinkedList[8] = timeData18;
-        timeDataLinkedList[9] = timeData19;
-        RecordTocoEcgView monitorTocoEcgView = (RecordTocoEcgView) findViewById(R.id.graph);
-        monitorTocoEcgView.setDatas(timeDataLinkedList);
+        handler = new Handler();
+
+        monitorTocoEcgView = (RecordTocoEcgView) findViewById(R.id.graph);
+        for (int i = 0; i < 1000; i++) {
+            final int temp = i;
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {// we add 100 new entries
+
+
+
+                    addEntry(temp);
+                }
+
+
+
+            }, 1000);
+        }
+
+    }
+    int max = 200;
+    int min = 100;
+    Random r = new Random();
+    private void addEntry(int i) {
+         Listener.TimeData timeDataLinkedList[]= new Listener.TimeData[1000];
+        int Result = r.nextInt(max-min) + min;
+            timeDataLinkedList[i] = new Listener.TimeData(Result,Result);
+            monitorTocoEcgView.setDatas(timeDataLinkedList);
+        }
+
+
+    @Override
+    public void notifyScrolled(int time) {
+        setRate(time);
+    }
+
+    private void setRate(int milliseconds) {
+        this.monitorTocoEcgView.setTime(milliseconds);
+        setRate(milliseconds / Listener.PRE, Listener.formatTime(milliseconds / 1000));
+    }
+
+    private void setRate(int index, String currStr) {
+        Log.d(index+"nikhil",currStr+"\n");
+//        this.timingTv.setText(new StringBuilder(String.valueOf(currStr)).append("/").append(this.toatTimeStr).toString());
+//        if (index < this.datas.length && index >= 0) {
+//            TimeData data = this.datas[index];
+//            if (data.heartRate < 50 || data.heartRate > 210) {
+//                this.rateTv.setText(getString(C0228R.string.data_none));
+//            } else {
+//                this.rateTv.setText(String.valueOf(data.heartRate));
+//            }
+//            this.currRate = data.heartRate;
+//            this.tocoTv.setText(String.valueOf(data.tocoWave));
+//        }
     }
 }
